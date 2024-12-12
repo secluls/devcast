c23e82ab423e6bcf0cb1ba0e90b328ababc9cc69
Stefanos Kornilios Mitsis Poiitidis <skmp@nilware.io>
2024-12-12 20:26:08 +0200

    persist

--- libswirl/input/mapping.cpp
+++ libswirl/input/mapping.cpp
@@ -35,6 +35,7 @@ button_list[] =
 	{ DC_DPAD2_DOWN, "dreamcast", "btn_dpad2_down" },
 	{ EMU_BTN_ESCAPE, "emulator", "btn_escape" },
 	{ EMU_BTN_MENU, "emulator", "btn_menu" },
+	{ EMU_BTN_SPEED_LIMIT, "emulator", "btn_toggle_fast" },
 	{ EMU_BTN_TRIGGER_LEFT, "compat", "btn_trigger_left" },
 	{ EMU_BTN_TRIGGER_RIGHT, "compat", "btn_trigger_right" },
 	{ EMU_BTN_STICK_LEFT, "compat", "btn_stick_left" },
